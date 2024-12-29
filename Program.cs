6d0021762711c53ab8ecefd44be84a935926b078
budoor <budooralsaidi1998@gmail.com>
2024-12-29 11:09:34 +0400

    testing connection

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace OutsourcingSystem
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);

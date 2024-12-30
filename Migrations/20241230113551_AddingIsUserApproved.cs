eed35c24500c46734b01987171866f4df188e8dc
Shahad <shahadalnaamani@outlook.com>
2024-12-30 13:20:55 +0100

    Adding IsUserApproved

---
+++ Migrations/20241230113551_AddingIsUserApproved.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OutsourcingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingIsUserApproved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsApproved",
+                table: "Users",
+                newName: "IsUserApproved");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsUserApproved",
+                table: "Users",
+                newName: "IsApproved");
+        }
+    }
+}
